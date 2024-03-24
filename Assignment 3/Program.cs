3193562edaa43ed213387d71953b58fbae984035
Sunil Patel <156108180+direct2sunny@users.noreply.github.com>
2024-03-24 11:41:09 -0400

    Update Program.cs

--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        RentalAgency rentalAgency = new RentalAgency();
+
+        Car car = new Car("Model S", "Tesla", 2020, 100m, 5, "Electric", "Automatic", true);
+        Truck truck = new Truck("F-150", "Ford", 2019, 120m, 1.5, "Pickup", true);
+        Motorcycle motorcycle = new Motorcycle("Ninja", "Kawasaki", 2018, 80m, 650, "Petrol", true);
+
+        rentalAgency.AddVehicle(car);
+        rentalAgency.AddVehicle(truck);
+        rentalAgency.AddVehicle(motorcycle);
+
+        rentalAgency.DisplayFleet();
+
+        rentalAgency.RentVehicle(car, car.RentalPrice);
+
+        Console.WriteLine($"Total Revenue: {rentalAgency.TotalRevenue}");
+    }
+}
